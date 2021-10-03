8fb9c6b9ba64a2b1d8f026820a345f6c3d1cf0ac
jaehyun <wogus3132@naver.com>
2021-10-03 14:50:17 +0900

    전달해준 문구를 메시지 확인활용

--- app/src/main/java/com/example/practiceintent/MainActivity.kt
+++ app/src/main/java/com/example/practiceintent/MainActivity.kt
@@ -9,6 +9,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sendMessageBtn.setOnClickListener {
+//         입력한 내용을 변수에 저장
+
+            val inputMessage =messageEdt.text.toString()
+
+            val myIntent = Intent(this,messageActivity ::class.java)
+
+            myIntent.putExtra("message",inputMessage)
+
+            startActivity(myIntent)
+
+        }
 
         moveToOtherBtn.setOnClickListener {
 
--- app/src/main/java/com/example/practiceintent/messageActivity.kt
+++ app/src/main/java/com/example/practiceintent/messageActivity.kt
@@ -2,10 +2,16 @@ package com.example.practiceintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class messageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+//        첨부된 데이터를 덱스트뷰에 저장
+        val intentMessage = intent.getStringExtra("message")
+
+        reciveMessageTxt.text = intentMessage
     }
 }
