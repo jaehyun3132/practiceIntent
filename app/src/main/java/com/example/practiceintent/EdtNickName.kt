3ef3f8ed4146048ec3213d412672b41a648d2ad0
jaehyun <wogus3132@naver.com>
2021-10-04 13:05:16 +0900

    닉네임 입력용 별개화면 추가

---
+++ app/src/main/java/com/example/practiceintent/EdtNickName.kt
@@ -0,0 +1,11 @@
+package com.example.practiceintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EdtNickName : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edt_nick_name)
+    }
+}
