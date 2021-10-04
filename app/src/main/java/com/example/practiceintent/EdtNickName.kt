74f86afcb28b31a58eb7c1c1b2e7099d70c61df8
jaehyun <wogus3132@naver.com>
2021-10-04 14:23:50 +0900

    닉네임 변경화면 입력후 완료화면으로 데이터 들고 돌아가기\\-
    -돌아간 메인화면에서는 별도로 작업 예정

--- app/src/main/java/com/example/practiceintent/EdtNickName.kt
+++ app/src/main/java/com/example/practiceintent/EdtNickName.kt
@@ -1,5 +1,6 @@
 package com.example.practiceintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edt_nick_name.*
@@ -11,6 +12,20 @@ class EdtNickName : AppCompatActivity() {
 
 
         okBtn.setOnClickListener {
+
+//            입력한 새 닉네임이 뭔지? 새로운 변수로 저장
+            val inputNewNickname = newNicknameEdt.text.toString()
+//            입력한 데이터를 들고서 메인화면으로 복귀.
+
+            val resultIntent = Intent()
+//입력한 닉네임을 달아주기 정도로만 사용하는 Intent다
+
+            resultIntent.putExtra("nickname",inputNewNickname)
+            setResult(RESULT_OK,resultIntent)
+            finish()
+
+
+
 
 
 
