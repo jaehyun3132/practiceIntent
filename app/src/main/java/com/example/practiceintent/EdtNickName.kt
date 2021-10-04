67a4a02b66d4fe54f2d531a73869f3f8aafd78cf
jaehyun <wogus3132@naver.com>
2021-10-04 13:34:55 +0900

    닉네임 변경을 위한 (startActivityForResult) 를 활용

--- app/src/main/java/com/example/practiceintent/EdtNickName.kt
+++ app/src/main/java/com/example/practiceintent/EdtNickName.kt
@@ -2,10 +2,18 @@ package com.example.practiceintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edt_nick_name.*
 
 class EdtNickName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edt_nick_name)
+
+
+        okBtn.setOnClickListener {
+
+
+
+        }
     }
 }
--- app/src/main/java/com/example/practiceintent/MainActivity.kt
+++ app/src/main/java/com/example/practiceintent/MainActivity.kt
@@ -6,12 +6,20 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val  REQUEST_FOR_NICKNAME = 1005
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         sendMessageBtn.setOnClickListener {
 //         입력한 내용을 변수에 저장
 
+            editNicknameBtn.setOnClickListener {
+
+                val myIntent = Intent(this, EdtNickName::class.java)
+                startActivityForResult(myIntent,REQUEST_FOR_NICKNAME)
+            }
             val inputMessage =messageEdt.text.toString()
 
             val myIntent = Intent(this,messageActivity ::class.java)
